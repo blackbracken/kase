6b594724491ace640d5b672706cb3d212b2f91ad
BlackBracken <me@bracken.black>
2018-12-11 14:27:53 +0900

    add SimpleWritableKey

---
+++ src/main/kotlin/black/bracken/kase/key/SimpleWritableKey.kt
@@ -0,0 +1,17 @@
+package black.bracken.kase.key
+
+import black.bracken.kase.container.SourceContainer
+
+/**
+ * @author BlackBracken
+ */
+class SimpleWritableKey<C : SourceContainer<S>, S : Any, V : Any>(
+    private val extractor: (C) -> V,
+    private val inserter: (C, V) -> Unit
+) : WritableKey<C, S, V> {
+
+    override fun extract(container: C): V = extractor(container)
+
+    override fun insert(container: C, value: V) = inserter(container, value)
+
+}
