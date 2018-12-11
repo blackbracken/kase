5c457874794608fbe2cddde39c3ea3a8b2d2f6a4
BlackBracken <me@bracken.black>
2018-12-11 19:06:17 +0900

    add methods to get value

--- src/main/kotlin/black/bracken/kase/Kase.kt
+++ src/main/kotlin/black/bracken/kase/Kase.kt
@@ -10,8 +10,42 @@ import black.bracken.kase.key.Key
  */
 interface Kase<C : SourceContainer<S>, S : Any> {
 
+    /**
+     * Returns a value corresponds the given [key] from [S].
+     *
+     * @throws NoSuchElementException if a value corresponds the given [key] is not exist.
+     */
     operator fun <V : Any> get(key: Key<C, S, V>): V
 
+    /**
+     * Sets the given [value] with the given [key] in [S].
+     *
+     * @throws IllegalStateException if the given [value] could not be set.
+     */
     operator fun <V : Any> set(key: Key<C, S, V>, value: V)
+
+    /**
+     * Returns a value corresponds the given [key] if it exists otherwise null.
+     */
+    fun <V : Any> getOrNull(key: Key<C, S, V>): V? = try {
+        this[key]
+    } catch (exception: NoSuchElementException) {
+        null
+    }
+
+    /**
+     * Returns a value corresponds the given [key] if it exists otherwise a result of [default] invoked.
+     */
+    fun <V : Any> getOrElse(key: Key<C, S, V>, default: () -> V): V = getOrNull(key) ?: default()
+
+    /**
+     * Returns a value corresponds the given [key] if it exists otherwise [defaultValue].
+     */
+    fun <V : Any> getOrDefault(key: Key<C, S, V>, defaultValue: V): V = getOrNull(key) ?: defaultValue
+
+    /**
+     * Returns a value corresponds the given [key] if it exists otherwise a result of [default] invoked and sets it in [S].
+     */
+    fun <V : Any> getOrPut(key: Key<C, S, V>, default: () -> V): V = getOrNull(key) ?: default().also { this[key] = it }
 
 }
