f29ca97d981855a80b32de92d7def732678d026b
BlackBracken <me@bracken.black>
2018-12-11 19:29:05 +0900

    add SimpleKase

---
+++ src/main/kotlin/black/bracken/kase/SimpleKase.kt
@@ -0,0 +1,18 @@
+package black.bracken.kase
+
+import black.bracken.kase.container.SourceContainer
+import black.bracken.kase.key.Key
+import black.bracken.kase.key.WritableKey
+
+/**
+ * @author BlackBracken
+ */
+class SimpleKase<C : SourceContainer<S>, S : Any>(private val container: C) : Kase<C, S> {
+
+    override fun <V : Any> get(key: Key<C, S, V>): V = key.extract(container)
+
+    override fun <V : Any> set(key: WritableKey<C, S, V>, value: V) = key.insert(container, value)
+
+}
+
+fun <C : SourceContainer<S>, S : Any> kaseOf(container: C): SimpleKase<C, S> = SimpleKase(container)
