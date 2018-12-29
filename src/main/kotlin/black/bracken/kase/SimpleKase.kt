7e12ca245acc478e6d3ef38a6899433fb1ed7380
BlackBracken <me@bracken.black>
2018-12-29 16:14:52 +0900

    fix type mismatch V? but V

--- src/main/kotlin/black/bracken/kase/SimpleKase.kt
+++ src/main/kotlin/black/bracken/kase/SimpleKase.kt
@@ -9,7 +9,7 @@ import black.bracken.kase.key.WritableKey
  */
 class SimpleKase<C : SourceContainer<S>, S : Any>(private val container: C) : Kase<C, S> {
 
-    override fun <V : Any> get(key: Key<C, S, V>): V = key.extract(container)
+    override fun <V : Any> get(key: Key<C, S, V>): V? = key.extract(container)
 
     override fun <V : Any> set(key: WritableKey<C, S, V>, value: V) = key.insert(container, value)
 
